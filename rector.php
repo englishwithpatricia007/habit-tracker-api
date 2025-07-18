c8046d80c4580ba5670decc95bca44d450a94f75
Patricia Nascimento <englishwithpatricia007@gmail.com>
2025-07-18 16:15:14 -0300

    Fomatando o código com pint.php

--- rector.php
+++ rector.php
@@ -1,10 +1,8 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 use Rector\Config\RectorConfig;
-
-use function PHPSTORM_META\type;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -16,11 +14,11 @@ return RectorConfig::configure()
     ])
     // uncomment to reach your current PHP version
     ->withPreparedSets(
-        deadCode:true,
-        codeQuality:true,
-        typeDeclarations:true,
-        privatization:true,
-        earlyReturn:true,
-        strictBooleans:true,
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        strictBooleans: true,
     )
     ->withPhpSets();
--- teste.php
+++ teste.php
@@ -1,2 +1,2 @@
 
-<?php echo "Servidor funcionando"; ?>
+<?php echo 'Servidor funcionando'; ?>
