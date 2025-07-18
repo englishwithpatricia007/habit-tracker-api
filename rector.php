d69dcba7ba54156c7c273e1ab83fb8a20b810136
Patricia Nascimento <englishwithpatricia007@gmail.com>
2025-07-18 15:58:59 -0300

    installing and setting up PHP Rector

---
+++ rector.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+use function PHPSTORM_META\type;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/app',
+        __DIR__ . '/bootstrap/app.php',
+        __DIR__ . '/database',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+    ])
+    // uncomment to reach your current PHP version
+    ->withPreparedSets(
+        deadCode:true,
+        codeQuality:true,
+        typeDeclarations:true,
+        privatization:true,
+        earlyReturn:true,
+        strictBooleans:true,
+    )
+    ->withPhpSets();
